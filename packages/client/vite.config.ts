11126a87c58a15b6cb61f405dfe650f99eea5c89
Veikkan <pierre.semanne@gmail.com>
2023-11-27 19:43:30 +0100

    init

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 3000,
+    fs: {
+      strict: false,
+    },
+  },
+  build: {
+    target: "es2022",
+    minify: true,
+    sourcemap: true,
+  },
+});
