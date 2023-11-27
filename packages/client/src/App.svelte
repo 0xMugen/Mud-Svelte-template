adc052df57538b89f9ccc16488eb0c31ba01898d
Veikkan <pierre.semanne@gmail.com>
2023-11-27 19:48:19 +0100

    feat: Add Mud to front page

--- packages/client/src/App.svelte
+++ packages/client/src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import mudLogo from "./assets/mud.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
@@ -12,20 +13,25 @@
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
+    <a href="https://mud.dev" target="_blank" rel="noreferrer">
+      <img src={mudLogo} class="logo mud" alt="Mud Logo" />
+    </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Vite + Svelte + Mud</h1>
 
   <div class="card">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
@@ -40,6 +46,9 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .logo.mud:hover {
+    filter: drop-shadow(0 0 2em #ff6430de);
   }
   .read-the-docs {
     color: #888;
